52aca772a62e671d6d2ca587bc0261d66f3de3c8
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-05 13:25:50 -0300

    update
    changed methods name

--- exercise-1/src/IBook.java
+++ exercise-1/src/IBook.java
@@ -1,6 +1,6 @@
 public interface IBook {
-    public void opening(); //Abrir
-    public void closing(); //Fechar
+    public void open(); //Abrir
+    public void close(); //Fechar
     public void leafThrough(int p); //Folhear
     public void nextPage(); //
     public void backPage();
