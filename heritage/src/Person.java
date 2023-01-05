7c4a5ada28cbda32c338f1ad5d1ea3f01ab193fd
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-05 19:57:26 -0300

    feat(Person): add Super class

---
+++ heritage/src/Person.java
@@ -0,0 +1,34 @@
+public class Person {
+    private String name;
+    private int age;
+    private char sex;
+
+    public void haveBirthday(){
+        setAge(getAge()+ 1);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+}
