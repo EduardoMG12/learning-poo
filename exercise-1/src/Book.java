a64dc1596571d9c3c62c34cce8ddb9fd29b4d719
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-05 02:15:57 -0300

    feat
    add new base class Book

--- exercise-1/src/Book.java
+++ exercise-1/src/Book.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class Book {
+public class Book {
+    private String title;
+    private String author;
+    private int totalPage;
+    private boolean open;
+    private Person reader;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setTotalPage(int totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public void setOpen(boolean open) {
+        this.open = open;
+    }
+
+    public void setReader(Person reader) {
+        this.reader = reader;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getTotalPage() {
+        return totalPage;
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
+
+    public Person getReader() {
+        return reader;
+    }
+    public void details(){
+
+    }
 }
