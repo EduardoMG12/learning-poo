227356a00bba443b52ad60a35c0d0604776cdec3
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-05 03:10:45 -0300

    update
    add implements and add interface methods, refactoring attributs and add constructor

--- exercise-1/src/Book.java
+++ exercise-1/src/Book.java
@@ -1,9 +1,11 @@
-public class Book {
+public class Book implements IBook {
     private String title;
     private String author;
     private int totalPage;
+    private int currentPage;
     private boolean open;
     private Person reader;
+
 
     public void setTitle(String title) {
         this.title = title;
@@ -16,9 +18,8 @@ public class Book {
     public void setTotalPage(int totalPage) {
         this.totalPage = totalPage;
     }
-
-    public void setOpen(boolean open) {
-        this.open = open;
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
     }
 
     public void setReader(Person reader) {
@@ -37,14 +38,67 @@ public class Book {
         return totalPage;
     }
 
-    public boolean isOpen() {
+    public int getCurrentPage(){
+        return currentPage;
+    }
+    public boolean getOpen() {
         return open;
     }
 
     public Person getReader() {
         return reader;
     }
-    public void details(){
+    public String details() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", totalPage=" + totalPage +
+                ", currentPage=" + currentPage +
+                ", open=" + open +
+                ", reader=" + reader +
+                '}';
+    }
 
+    @Override
+    public void opening() {
+        if(this.open == false){
+            this.open = true;
+            System.out.println("[open book]");
+        }
+    }
+
+    @Override
+    public void closing() {
+        if(getOpen()){
+            this.open = false;
+            System.out.println("[close book]");
+        }
+    }
+
+    @Override
+    public void leafThrough(int p) {
+            this.currentPage = p;
+            System.out.println(this.currentPage);
+    }
+
+    @Override
+    public void nextPage() {
+            this.currentPage++;
+            System.out.println(this.currentPage);
+    }
+
+    @Override
+    public void backPage() {
+            this.currentPage--;
+            System.out.println(this.currentPage);
+    }
+
+    public Book(String title, String author, int totalPage, Person reader) {
+        this.title = title;
+        this.author = author;
+        this.totalPage = totalPage;
+        this.currentPage = 0;
+        this.open = false;
+        this.reader = reader;
     }
 }
