f223d7c9f6dbb8c40a8bf125063a73ff744fd984
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-05 03:11:29 -0300

    refactoring
    refactoring methods

--- exercise-1/src/IBook.java
+++ exercise-1/src/IBook.java
@@ -1,7 +1,7 @@
 public interface IBook {
-    public void open(); //Abrir
-    public void close(); //Fechar
-    public void leafThrough(); //Folhear
+    public void opening(); //Abrir
+    public void closing(); //Fechar
+    public void leafThrough(int p); //Folhear
     public void nextPage(); //
     public void backPage();
 
