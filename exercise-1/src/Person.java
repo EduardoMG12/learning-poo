0643ce8b583918c55d5377eb3a7225e8f6de1a77
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-05 02:11:38 -0300

    feat
    add new class 'Person'

---
+++ exercise-1/src/Person.java
@@ -0,0 +1,34 @@
+public class Person {
+    private String name;
+    private int years;
+    private char sex;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setYears(int years) {
+        this.years = years;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYears() {
+        return years;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void haveBirthday(){
+
+
+    }
+}
