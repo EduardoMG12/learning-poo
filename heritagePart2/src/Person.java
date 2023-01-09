b3a76ecaece0f908e5cde17ee7428300b9c2a28e
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-09 16:31:26 -0300

    feat(classRoot):Person

---
+++ heritagePart2/src/Person.java
@@ -0,0 +1,39 @@
+public abstract class Person {
+    private String name;
+    private int age;
+    private char sex;
+
+    public Person(String name, int age, char sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    final public void haveBirthday(){
+       setAge(getAge() + 1);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+}
