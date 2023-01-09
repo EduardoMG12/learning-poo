ecdcb1d45ffba42b99251ed0c9de4ffc6e9bbac8
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-09 16:35:17 -0300

    test(examples): add examples intance

--- heritagePart2/src/Main.java
+++ heritagePart2/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        var visitor = new Visitor("Eduarda",17,'F');
+        var scholar = new Scholar("Eduardo", 17,'M',12445,"Business",1324);
+        var student = new Student("Charles", 20, 'M',1345,"Programming");
     }
 }
