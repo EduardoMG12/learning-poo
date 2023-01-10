2c75296177515bc1da3b66ffd5668d95d9528d4d
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-10 15:06:02 -0300

    update(dog): examples overload

--- overridingPolymorphism/src/Dog.java
+++ overridingPolymorphism/src/Dog.java
@@ -1,11 +1,31 @@
-public class Dog extends Mammals{
+public class Dog extends Wolf{
     public Dog(float weight, int age, int members, String furColor) {
         super(weight, age, members, furColor);
     }
-    public void buryBone(){
-        System.out.println("burying bone");
-    }
-   public void waggingTail(){
-       System.out.println("wagging the tail");
+//examples polymorphism overload
+   public void react(String speaks){
+        if(speaks == "positive"){
+            this.waggingTail();
+        }else this.growl();
+   }
+
+   public void react(int hour){
+        if (hour < 20 && hour > 8){
+            this.waggingTail();
+        }else this.growl();
+   }
+
+   public void react(boolean owner){
+        if (owner){
+            this.waggingTail();
+        }else this.growl();
+   }
+
+   public void react(){
+        if (getAge() < 5  ){
+            if (getWeight() < 10) this.waggingTail();
+            else this.makeSound();
+        }else System.out.println("ignore");
+
    }
 }
